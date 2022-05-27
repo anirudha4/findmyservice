3438c8cf1472da9e8467797ce99bf3ade051ca11
anirudhag1999 <anirudha@chisellabs.com>
2022-05-27 20:06:52 +0530

    initialize firebase admin sdk

---
+++ fire/index.js
@@ -0,0 +1,15 @@
+import admin from 'firebase-admin'
+import serviceAccount from './serviceAccount.json'
+
+try {
+    admin.initializeApp({
+        credential: admin.credential.cert(serviceAccount),
+    })
+} catch (error) {
+    if (!/already exists/u.test(error.message)) {
+        console.error('Firebase admin initialization error', error.stack)
+    }
+}
+export const db = admin.firestore();
+
+export default admin;
